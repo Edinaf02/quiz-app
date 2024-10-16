d9fe4cb9215b625b6a0bac62fd224ef8ce2e6a6f
Edom Feleke <edinafeleke@gmail.com>
2024-10-16 23:11:20 +0300

    3rd successful commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="bg-purple-700 text-white p-4 text-center">
-      <p>&copy; {10().2024()} Edinaf. All rights reserved.</p>
+      <p>&copy; {new Date().getFullYear()} Edinaf. All rights reserved.</p>
     </footer>
   );
 };
